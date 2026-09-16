830b05a54aa6a6cc148e4abf3a36ee06436367f1
Maria Paula Baca <mariabaca2004@gmail.com>
2026-09-16 15:48:52 -0500

    Interfaz inicial del explorador de clima

---
+++ script.js
@@ -0,0 +1,4 @@
+const inputCiudad = document.getElementById("inputCiudad");
+const btnBuscar = document.getElementById("btnBuscar");
+const resultado = document.getElementById("resultado");
+const mensaje = document.getElementById("mensaje");
