0a0f8ba6d33c0cb77c04e3b832a2ee74952ae049
Maria Paula Baca <mariabaca2004@gmail.com>
2026-09-16 21:04:50 -0500

    Agrega configuración y conexión con API

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const inputCiudad = document.getElementById("inputCiudad");
 const btnBuscar = document.getElementById("btnBuscar");
 const resultado = document.getElementById("resultado");
 const mensaje = document.getElementById("mensaje");
+
+
+async function buscarDatos(ciudad) {
+
+    const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${encodeURIComponent(ciudad)}&lang=es`;
+
+    const respuesta = await fetch(URL);
+
+    if (!respuesta.ok) {
+        throw new Error("No se pudo obtener la información del clima.");
+    }
+
+    const datos = await respuesta.json();
+
+    console.log(datos);
+}
+
+
+btnBuscar.addEventListener("click", () => {
+
+    const ciudad = inputCiudad.value;
+
+    buscarDatos(ciudad);
+
+});
